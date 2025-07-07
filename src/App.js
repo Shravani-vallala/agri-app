afe0314a52897274a95ce602da8b2910a93b26ed
venky-1234 <ambalavenkataramana2@gmail.com>
2025-07-07 22:10:12 +0530

    Default page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
 import Dashboard from './Dashboard';
+import FarmerEquipmentsPage from './FarmerEquipmentsPage';
+
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={< FarmerEquipmentsPage/>} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/Login" element={< Login/>} />
+
 
       </Routes>
     </Router>
